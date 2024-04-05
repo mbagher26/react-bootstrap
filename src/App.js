62b3c98d98659b03d8d3ef1b430385161b6d2ec1
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-05 11:00:41 -0400

    import Row and Col from react-bootstrap $ import Home component $ placement Home component inside Col

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import Menous from './components/menous/Menous';
+import Home from './components/home/Home';
+
 
 function App() {
   return (
     <div className="App">
       <Container fluid style={{padding:0}}>
-        <Menous/>
+        <Menous />
+        <Row>
+          <Col><Home/></Col>
+        </Row>
       </Container>
     </div>
   );
