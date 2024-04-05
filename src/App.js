2182c35140b780c9db2a2e8813199e7ba215638a
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-05 11:43:02 -0400

    new Col for Login component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Col, Container, Row } from 'react-bootstrap';
 import Menous from './components/menous/Menous';
 import Home from './components/home/Home';
+import Login from './components/login/Login';
 
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
       <Container fluid style={{padding:0}}>
         <Menous />
         <Row>
-          <Col lg={6}><Home/></Col>
+          <Col lg={6}><Login/></Col>
+          <Col lg={6}><Home /></Col>
         </Row>
       </Container>
     </div>
