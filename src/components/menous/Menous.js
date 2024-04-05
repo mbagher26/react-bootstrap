c4e08e6ab01c9993cbc26539d5421eac1c2cf5b2
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-05 11:04:53 -0400

    update import Menous.css

--- src/components/menous/Menous.js
+++ src/components/menous/Menous.js
@@ -5,7 +5,7 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import './styles/Menous.css'
+import './../styles/Menous.css'
 
 
 export default class Menous extends Component {
