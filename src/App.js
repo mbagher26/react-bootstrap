813971bd316cd4f217b2f71a8b5125894dd9fd1e
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-05 09:24:00 -0400

    import Container tag and Menous component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { Container } from 'react-bootstrap';
+import Menous from './components/menous/Menous';
 
 function App() {
   return (
     <div className="App">
-      
+      <Container fluid style={{padding:0}}>
+        <Menous/>
+      </Container>
     </div>
   );
 }
